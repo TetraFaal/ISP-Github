5f417340973ccdf1ed631403796cf53ee56559c9
Tetra <rogi.1107@live.com>
2020-09-27 10:49:16 +0200

    Added execercise files

---
+++ TP1/exerciseMaterial/Exercise6_example.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This is the starting file for doing Exercise 6.
+"""
+
+hashed_phone_numbers = {
+    "participants": [
+        {
+            "work phone": "8a2e120289733c9ae701df076f85c899",
+            "mobile phone": "82056ad09062d50b22f57e355951637c",
+            "contacts": ["c6f12c4a85e85529aa90614dece312a8", "0b5a4e6be2c5693896e7ea35b99d75a7", "312cf2f91d00bfdf44168b0dc759aabc"]
+        },
+        {
+            "work phone": "e6baa213a602c6f30427a92ad019619b",
+            "mobile phone": "7c2a46374d7746b37b36d3588cfd2246",
+            "contacts": ["8bbbcd609d0310d0a7af7f2b9d51ba64", "9f1469d4fd3ab018ed358b1ca835888d"]
+        }
+    ]
+}
+
+
+if __name__ == "__main__":
+    pass
